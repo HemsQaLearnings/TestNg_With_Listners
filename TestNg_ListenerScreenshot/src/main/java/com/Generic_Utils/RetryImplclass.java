ad5c450c55b068561a0ce1b433416e12882a31f0
HemsQaLearnings <hemsqalearnings@gmail.com>
2025-01-20 19:53:04 +0530

    Update RetryImplclass.java

--- TestNg_ListenerScreenshot/src/main/java/com/Generic_Utils/RetryImplclass.java
+++ TestNg_ListenerScreenshot/src/main/java/com/Generic_Utils/RetryImplclass.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryImplclass implements IRetryAnalyzer {
 
 	int count=0;
-	int retryLimit=2;
+	int retryLimit=0;
 	
 	public boolean retry(ITestResult result)
 	{
